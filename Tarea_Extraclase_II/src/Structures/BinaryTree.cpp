2b8cdf1cfaef6ebd6684d2fdb2368ae388ff33fd
Paola <pvillegasch@gmail.com>
2019-08-11 22:08:34 -0600

    Metodo para insertar nodos agregado

--- Tarea_Extraclase_II/src/Structures/BinaryTree.cpp
+++ Tarea_Extraclase_II/src/Structures/BinaryTree.cpp
@@ -1,20 +1,18 @@
 #include "TreeNode.cpp"
+using namespace std;
 
 class BinaryTree {
     private:
         TreeNode *root;
-        TreeNode * addRecursive(TreeNode *current, int value) {
-
-        };
-        TreeNode * deleteRecursive(TreeNode *current, int value) {
-
-        };
+        TreeNode * addRecursive(TreeNode *current, int value);
+        // TreeNode * deleteRecursive(TreeNode *current, int value);
+        void inorderRecursive(TreeNode *current);
         
     public:
         BinaryTree();
         void add(int value);
-        void deleteValue(int value);
-        void preorder();
+        // void deleteValue(int value);
+        void inorder();
 
 };
 
@@ -22,10 +20,59 @@ BinaryTree::BinaryTree(){
     root = NULL;
 }
 
+TreeNode * BinaryTree::addRecursive(TreeNode *current, int value) {
+    if (current == NULL) {
+        //se encontro el lugar para insertar el nodo
+        return new TreeNode(value);
+    } else if (current->getValue() > value) {
+        current->setLeftChild(addRecursive(current->getLeftChild(), value));
+    } else if (current->getValue() < value) {
+        current->setRightChild(addRecursive(current->getRightChild(), value));
+    } 
+     //sino el elemento ya existe
+
+     return current;
+} 
+
 void BinaryTree::add(int value) {
     root = addRecursive(root, value);
 }
 
-void BinaryTree::deleteValue(int value){
-    root = deleteRecursive(root, value);
+// TreeNode * deleteRecursive(TreeNode *current, int value) {
+//     return NULL;
+// }
+
+// void BinaryTree::deleteValue(int value){
+//     root = deleteRecursive(root, value);
+// }
+
+void BinaryTree::inorderRecursive(TreeNode *current) {
+    if (current == NULL) {
+        return;
+    }
+    inorderRecursive(current->getLeftChild());
+    cout << current->getValue() << " ";
+    inorderRecursive(current->getRightChild());
 }
+
+void BinaryTree::inorder() {
+    inorderRecursive(root);
+
+}
+
+int main(int argc, char *argv[]) {
+    BinaryTree *tree = new BinaryTree();
+
+    tree->add(5);
+    tree->add(51);
+    tree->add(10);
+    tree->add(5);
+    tree->add(2);
+    tree->add(1);
+
+    tree->inorder();
+    cout << "\n";
+    
+    return 0;
+}
+
